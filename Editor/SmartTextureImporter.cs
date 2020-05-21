edbbacc8a360fe2b606e889194bf2d38348c3f00
CianNoonan <cian.noonan23@gmail.com>
2020-05-21 11:59:25 +0100

    Default TextureImporterPlatformSettings to be new inst to avoid nullref potential on creation of asset

--- Editor/SmartTextureImporter.cs
+++ Editor/SmartTextureImporter.cs
@@ -33,7 +33,7 @@ public class SmartTextureImporter : ScriptedImporter
     [SerializeField] TextureWrapMode m_WrapMode = TextureWrapMode.Repeat;
     [SerializeField] int m_AnisotricLevel = 1;
 
-    [SerializeField] TextureImporterPlatformSettings m_TexturePlatformSettings = null;
+    [SerializeField] TextureImporterPlatformSettings m_TexturePlatformSettings = new TextureImporterPlatformSettings();
 
     [SerializeField] TextureFormat m_TextureFormat = TextureFormat.ARGB32;
     [SerializeField] bool m_UseExplicitTextureFormat = false;
